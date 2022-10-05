df19ebc5a7572a5ef4b22b0379cf1f1bcdd82cfd
Luo <852539882@qq.com>
2022-10-05 10:32:12 +0800

    Second time commit

--- src/main/java/com/gdpu/git/GitTest.java
+++ src/main/java/com/gdpu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.gdpu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellogit!");
+        System.out.println("hellogithub");
     }
 }
